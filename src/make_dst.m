8d9bbd3c563a2e494be8e04e16c1dd50ab9a3677
Martin Vejmelka <vejmelkam@gmail.com>
2013-10-31 17:52:25 -0600

    added discrete sine transform

---
+++ src/make_dst.m
@@ -0,0 +1,8 @@
+
+% Synopsis:  F = make_dct(n)
+% Returns the n x n DST transform matrix.
+% Definition looked up on http://www.mathworks.com/help/pde/ug/dst.html
+
+function F = make_dst(n)
+    F = sqrt(2/(n+1))*sin((pi/(n+1))*([1:n]'*[1:n]));
+end
