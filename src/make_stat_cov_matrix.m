b8f3b002273602e95d0fb5eb17a8e6394f2d1f0b
Martin Vejmelka <vejmelkam@gmail.com>
2013-11-04 12:16:16 -0700

    ranemd make_stat_covar to make_stat_cov_matrix for consistency

--- src/make_stat_cov_matrix.m
+++ src/make_stat_cov_matrix.m
@@ -5,7 +5,7 @@
 %  vectors of dimensionality n.  The form of the covariance depends on the
 %  choice of the periodic boundary conditions (bc_func).
 %
-%  C = make_stat_covar(cs, n, bc_func)
+%  C = make_stat_cov_matrix(cs, n, bc_func)
 %
 %  cs - the stationary covariance representation cs in the form c(0), c(1),
 %       ... c(m)
@@ -13,9 +13,9 @@
 %  bc_func - the function used to periodically extend the vectors (must
 %       match choice of basis vectors - dst,dct,fft)
 
-function C = make_stat_covar(cs, n, bc_func)
+function C = make_stat_cov_matrix(cs, n, bc_func)
     m = length(cs) - 1;
-    Cb = cov_oper_to_matrix(cs, n);
+    Cb = make_band_cov_oper(cs, n);
     E = bc_func(eye(n),m);
     C = Cb * E;
 end
