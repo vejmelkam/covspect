63a5a7f2292316c1c7c19b469651b72c31f1252f
Martin Vejmelka <vejmelkam@gmail.com>
2013-11-05 19:51:02 -0700

    ensured reporting of normalized results so they can be compared across smoothness levels

--- exp/exp_finite_stationary_diagonal_approximation_all.m
+++ exp/exp_finite_stationary_diagonal_approximation_all.m
@@ -6,12 +6,13 @@
 %  spaces, such as the Fourier, DST, ... or wavelet spaces in 1D.
 %
 %
+clear;
 
 %% Parameters
 
-pn = 256;                     % dimensionality of spaces (no. of grid points)
+pn = 128;                     % dimensionality of spaces (no. of grid points)
 pN = 1:2:10;                  % ensemble sizes
-iters = 10;                   % number of iterations with each configuration
+iters = 20;                   % number of iterations with each configuration
 palpha = logspace(-2, 1, 10); % smoothness of covariance function
 
 % use the following methods for approximating sample covariance
@@ -20,7 +21,7 @@ pmethods = { 'Sample', 'DST', 'DCT', 'FFT', 'Coiflet' };  % use the following xf
 pxf_funcs = {@(n) error('this should not be called'), @dst_matrix, @dct_matrix, @fft_matrix, @wav_coi_matrix };
 
 pF = {};
-for i=1:length(pmethods)
+for i=2:length(pmethods)
     f = pxf_funcs{i};
     pF{i} = f(pn);
 end
@@ -30,11 +31,12 @@ results = zeros(length(palpha), length(pN), length(pmethods), iters);
 for alpha_ndx=1:length(palpha)
     alpha = palpha(alpha_ndx);
     fprintf('Running experiment for alpha = %g\n', alpha);
-    cs = exp(-(0:pn-1)/alpha);
+    cs = exp(-(0:pn-1)*alpha);
     C = make_finite_cov_matrix_from_cs(cs,pn);
     if any(eig(C) <= 0)
         error('Matrix not positive definite for alpha=%g', alpha);
     end
+    norm_C = norm(C, 'fro');
     C_05 = sqrtm(C);
     for N_ndx=1:length(pN)
         N = pN(N_ndx);
@@ -43,12 +45,11 @@ for alpha_ndx=1:length(palpha)
             Y = randn(pn,N);
             X = C_05*Y;
             C_N = 1/N*(X*X');
-            results(alpha_ndx, N_ndx, 1, iter, 1) = norm(C_N - C, 'fro') / norm(C, 'fro');
+            results(alpha_ndx, N_ndx, 1, iter) = norm(C_N - C, 'fro') / norm_C;
             for meth_id=2:length(pmethods)
                 % matrix transforming grid space to spectral/wavelet space
                 F = pF{meth_id};
                 C_F = F*C*F';
-                norm_C_F = norm(C_F, 'fro');
                 
                 % sample covariance in spectral/wavelet space
                 C_FN = F*C_N*F;
@@ -57,7 +58,7 @@ for alpha_ndx=1:length(palpha)
                 D_FN = diag(diag(C_FN));
 
                 % compute errors
-                results(alpha_ndx, N_ndx, meth_id, iter) = norm(D_FN - C_F, 'fro') / norm_C_F;
+                results(alpha_ndx, N_ndx, meth_id, iter) = norm(D_FN - C_F, 'fro') / norm_C;
             end
         end
     end
