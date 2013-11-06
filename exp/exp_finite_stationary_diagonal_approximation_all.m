60440eceeefb946219f46c230c75c185ab7fdce9
Martin Vejmelka <vejmelkam@gmail.com>
2013-11-05 19:36:14 -0700

    modified exp_finite_stationary_diagonal_approximation_all and plot_* to improve speed

--- exp/exp_finite_stationary_diagonal_approximation_all.m
+++ exp/exp_finite_stationary_diagonal_approximation_all.m
@@ -10,11 +10,14 @@
 %% Parameters
 
 pn = 256;                     % dimensionality of spaces (no. of grid points)
-pN = 1:5;                     % ensemble sizes
+pN = 1:2:10;                  % ensemble sizes
 iters = 10;                   % number of iterations with each configuration
 palpha = logspace(-2, 1, 10); % smoothness of covariance function
-pmethods = { 'DST', 'DCT', 'FFT', 'Coiflet' };  % use the following xforms
-pxf_funcs = {@dst_matrix, @dct_matrix, @fft_matrix, @wav_coi_matrix };
+
+% use the following methods for approximating sample covariance
+% NOTE: 'Sample' MUST be first!
+pmethods = { 'Sample', 'DST', 'DCT', 'FFT', 'Coiflet' };  % use the following xforms
+pxf_funcs = {@(n) error('this should not be called'), @dst_matrix, @dct_matrix, @fft_matrix, @wav_coi_matrix };
 
 pF = {};
 for i=1:length(pmethods)
@@ -22,43 +25,39 @@ for i=1:length(pmethods)
     pF{i} = f(pn);
 end
 
-results = zeros(length(palpha), length(pN), length(pmethods), iters, 2);
+results = zeros(length(palpha), length(pN), length(pmethods), iters);
 
 for alpha_ndx=1:length(palpha)
     alpha = palpha(alpha_ndx);
-    fprintf('Running experiment for alpha = %g', alpha);
-    cs = exp(-(0:pn-1)*alpha);
+    fprintf('Running experiment for alpha = %g\n', alpha);
+    cs = exp(-(0:pn-1)/alpha);
     C = make_finite_cov_matrix_from_cs(cs,pn);
     if any(eig(C) <= 0)
         error('Matrix not positive definite for alpha=%g', alpha);
     end
+    C_05 = sqrtm(C);
     for N_ndx=1:length(pN)
         N = pN(N_ndx);
         for iter=1:iters
             % Y is an N(0,I)-distributed ensemble (elements in column)
             Y = randn(pn,N);
-            for meth_id=1:length(pmethods)
+            X = C_05*Y;
+            C_N = 1/N*(X*X');
+            results(alpha_ndx, N_ndx, 1, iter, 1) = norm(C_N - C, 'fro') / norm(C, 'fro');
+            for meth_id=2:length(pmethods)
                 % matrix transforming grid space to spectral/wavelet space
                 F = pF{meth_id};
                 C_F = F*C*F';
                 norm_C_F = norm(C_F, 'fro');
-
-                % compute symmetric square root
-                C_F_05 = sqrtm(C_F);
-
-                % generate ensemble
-                X = C_F_05 * Y;
-
-                % compute sample covariance estimate (already in spectral
-                % space)
-                C_FN = 1/N*X*X';
+                
+                % sample covariance in spectral/wavelet space
+                C_FN = F*C_N*F;
 
                 % diagonalize
                 D_FN = diag(diag(C_FN));
 
                 % compute errors
-                results(alpha_ndx, N_ndx, meth_id, iter, 1) = norm(C_FN - C_F, 'fro') / norm_C_F;
-                results(alpha_ndx, N_ndx, meth_id, iter, 2) = norm(D_FN - C_F, 'fro') / norm_C_F;
+                results(alpha_ndx, N_ndx, meth_id, iter) = norm(D_FN - C_F, 'fro') / norm_C_F;
             end
         end
     end
--- exp/plot_finite_stationary_diagonal_approximation_all_1.m
+++ exp/plot_finite_stationary_diagonal_approximation_all_1.m
@@ -5,23 +5,14 @@
 figure(1);
 for N_ndx=1:length(pN)
     N = pN(N_ndx);
-    subplot(2,length(pN),N_ndx);
+    subplot(2,3,N_ndx);
     semilogx(palpha, mean(squeeze(results(:,N_ndx,1,:,1)),2), 'ro-'); hold on;
     semilogx(palpha, mean(squeeze(results(:,N_ndx,2,:,1)),2), 'go-');
     semilogx(palpha, mean(squeeze(results(:,N_ndx,3,:,1)),2), 'bo-');
-    semilogx(palpha, mean(squeeze(results(:,N_ndx,4,:,1)),2), 'ko-'); hold off;
+    semilogx(palpha, mean(squeeze(results(:,N_ndx,4,:,1)),2), 'ko-');
+    semilogx(palpha, mean(squeeze(results(:,N_ndx,5,:,1)),2), 'mo-'); hold off;
     legend(pmethods);
     xlabel('alpha [-]');
-    ylabel('Sample cov. Frobenius error');
+    ylabel('Cov. estimate Frobenius error');
     title(sprintf('Ensemble size %d - Sample cov.', N));
-
-    subplot(2,length(pN),N_ndx + length(pN));
-    semilogx(palpha, mean(squeeze(results(:,N_ndx,1,:,2)),2), 'ro-'); hold on;
-    semilogx(palpha, mean(squeeze(results(:,N_ndx,2,:,2)),2), 'go-');
-    semilogx(palpha, mean(squeeze(results(:,N_ndx,3,:,2)),2), 'bo-');
-    semilogx(palpha, mean(squeeze(results(:,N_ndx,4,:,2)),2), 'ko-'); hold off;
-    legend(pmethods);
-    xlabel('alpha [-]');
-    ylabel('Diag. sample cov. Frobenius error');
-    title(sprintf('Ensemble size %d - Diag. sample cov.', N));
 end
