a2bef1a11e90f78bd75c320126c4963e7eedd7b5
Ivan Kasanicky <kasanickyivan@gmail.com>
2013-11-08 14:31:36 -0700

    added implementation of EnKF using diagonal spectral sample covariance estimate and test file using lorenz93 model

---
+++ enkf/cov_xform.m
@@ -0,0 +1,18 @@
+
+%%
+%   Function for estimating sample covariance in spectral space 
+%   and reducing it by given mask. 
+%
+%   function Q=cov_xform(X,F,M)
+%
+%   X - enseble (ensemble members in columns)
+%   F - transformation matrix
+%   M - mask matrix
+%%
+
+function Q=cov_xform(XF,F,M)
+C=cov(XF');
+CF=F*C*F';
+Q=CF.*M;
+Q=F'*Q*F;
+end
---
+++ enkf/enkf.m
@@ -0,0 +1,40 @@
+
+%%
+%   Standart EnKF. 
+%
+%   Implemenation based on J. Mandel: A  Brief Tutorial on 
+%   the Ensemble Kalman Filter 
+%   http://arxiv.org/abs/0901.3725
+%
+%
+%   function XA=enkf(XF,H,d,r,f_cov_est)
+%
+%   XF - forecast ensemble
+%   H - observation operator
+%   d - observed data
+%   r - variance of observed data
+%   f_cov_est - function for estimating forecast covariance - function
+%   handle
+%%
+
+function XA=enkf(XF,H,d,r,f_cov_est)
+    %   n - size of state
+    %   N - number of enseble members
+    N = size(XF,2);
+    %   observed data length
+    d_l = length(d);
+    R = diag(r);
+    %   input data perturbation
+    D = repmat(d,1,N) + diag(sqrt(r))*randn(d_l,N);
+    
+    % forecast covarince
+    QF = f_cov_est(XF);
+    
+    % Kalman gain matrix
+    M = (H * QF * H' + R);
+    Mi = inv(M);
+    K = QF*H*Mi;
+    
+    % Analysis
+    XA=XF+K*(D-H*XF);
+end
